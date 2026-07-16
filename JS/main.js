f5f433e9a34973cfd3b67d2c5b9a45059874c8ac
Bluee <juanpispinilla4@gmail.com>
2026-07-15 19:58:58 -0500

    scroll reveal GSAP, fix responsive movil, sobre-mi completo

--- JS/main.js
+++ JS/main.js
@@ -26,3 +26,25 @@ gsap.fromTo(".hero-sub",
     { opacity: 0 },
     { duration: 2, opacity: 1, delay: 0.4, ease: "power2.out" }
 )
+
+// Registrar ScrollTrigger
+gsap.registerPlugin(ScrollTrigger)
+
+ScrollTrigger.refresh()
+
+// Animacion de entrada para articulos
+gsap.utils.toArray("article").forEach(function(article) {
+    gsap.fromTo(article,
+        { opacity: 0, y: 50 },
+        {
+            opacity: 1,
+            y: 0,
+            duration: 0.7,
+            ease: "power2.out",
+            scrollTrigger: {
+                trigger: article,
+                start: "top 75%",
+            }
+        }
+    )
+})
